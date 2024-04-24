c0201d845e6e8c52e6af461ca30d255562cf89ac
Serra Tarımer <serraoner18@gmail.com>
2024-04-24 23:32:06 +0300

    Daire dilimi alanı hesaplama

---
+++ src/week1/CircleAreaCalculator.java
@@ -0,0 +1,24 @@
+package week1;
+
+import java.util.Scanner;
+
+public class CircleAreaCalculator
+{
+    public static void main(String[] args) {
+        double radius, alpha;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter circle radius: ");
+        radius = inp.nextDouble();
+        System.out.print("Enter circle center angle: ");
+        alpha = inp.nextDouble();
+
+        double circleArea          = Math.PI * radius * radius;
+        double circleCircumference = 2 * Math.PI * radius;
+        double partialCircleArea   = circleArea * alpha / 360.0;
+
+        System.out.println("Total circle area: " + circleArea);
+        System.out.println("Total circle circumference: " + circleCircumference);
+        System.out.println("Partial circle area: " + partialCircleArea);
+    }
+}

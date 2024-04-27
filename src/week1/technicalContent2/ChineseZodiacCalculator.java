d8c1b63f9c2a85959f11110fbd59a72836993690
Serra Tarımer <serraoner18@gmail.com>
2024-04-27 16:13:32 +0300

    Çin zodyağı ödevi

---
+++ src/week1/technicalContent2/ChineseZodiacCalculator.java
@@ -0,0 +1,56 @@
+package week1.technicalContent2;
+
+import java.util.Scanner;
+
+public class ChineseZodiacCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = inp.nextInt();
+
+        switch (year % 12)
+        {
+            case 0:
+                System.out.println("Monkey");
+                break;
+            case 1:
+                System.out.println("Rooster");
+                break;
+            case 2:
+                System.out.println("Dog");
+                break;
+            case 3:
+                System.out.println("Pig");
+                break;
+            case 4:
+                System.out.println("Rat");
+                break;
+            case 5:
+                System.out.println("Ox");
+                break;
+            case 6:
+                System.out.println("Tiger");
+                break;
+            case 7:
+                System.out.println("Rabbit");
+                break;
+            case 8:
+                System.out.println("Dragon");
+                break;
+            case 9:
+                System.out.println("Snake");
+                break;
+            case 10:
+                System.out.println("Horse");
+                break;
+            case 11:
+                System.out.println("Sheep");
+                break;
+            default:
+                System.out.println("Invalid year");
+        }
+    }
+}
